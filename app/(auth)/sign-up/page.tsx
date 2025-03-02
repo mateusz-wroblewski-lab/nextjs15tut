d57ff320ecb2060f532bd5bfbfca86500a13667e
mateusz-wroblewski-lab <155849345+mateusz-wroblewski-lab@users.noreply.github.com>
2025-03-02 21:15:42 +0100

    Update page.tsx in signUp

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,16 +3,20 @@
 import React from "react";
 import AuthForm from "@/components/forms/AuthForm";
 import { SignUpSchema } from "@/lib/vaidations";
+import { signUp } from "@/lib/actions/auth";
 
-const SignUp = () => {
-  return (
+const SignUp = () => (
     <AuthForm
       formType="SIGN_UP"
       schema={SignUpSchema}
-      defaultValues={{ email: "", password: "", name: "", username: "" }}
-      onSubmit={(data) => Promise.resolve({ success: true, data })}
+      defaultValues={{ 
+        email: "", 
+        password: "", 
+        name: "", 
+        username: "",
+      }}
+      onSubmit={signUp}
     />
   );
-};
 
 export default SignUp;
